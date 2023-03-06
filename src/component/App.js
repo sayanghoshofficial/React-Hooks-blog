1816525eabb65b72cf9638bf795fe09c3ecebfc0
Sayan Ghosh <sayanghosh379@gmail.com>
2023-03-06 19:45:34 +0530

    rendering routes for all nessary pages

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,22 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+// import {Navbar, Home, CreatePost, PostDetail} from './';
+import Navbar from "./Navbar";
+import Home from "./Home";
+import PostDetail from "./PostDetail";
+import CreatePost from "./CreatePost";
 
 function App() {
   return (
-    <div className="App">
-     Starting React Blog
+    <div className="container">
+      <Navbar/>
+      <Router>
+        <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/post/:postId" element={<PostDetail/>}/>
+            <Route exact path="/create-post" element={<CreatePost/>}/>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
