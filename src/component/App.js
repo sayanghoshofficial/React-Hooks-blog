6cd3a220efee0fb4d040517c075ca09e5f4587e2
Sayan Ghosh <sayanghosh379@gmail.com>
2023-03-06 21:04:24 +0530

    create navbar and create from for create post

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 // import {Navbar, Home, CreatePost, PostDetail} from './';
-import Navbar from "./Navbar";
+import Navbar  from "./Navbar";
 import Home from "./Home";
 import PostDetail from "./PostDetail";
 import CreatePost from "./CreatePost";
@@ -8,8 +8,9 @@ import CreatePost from "./CreatePost";
 function App() {
   return (
     <div className="container">
-      <Navbar/>
+     
       <Router>
+        <Navbar/>
         <Routes>
             <Route exact path="/" element={<Home/>}/>
             <Route exact path="/post/:postId" element={<PostDetail/>}/>
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 function NavBar() {
-    return (
-      <div className="">
-        NavBar
-      </div>
-    );
-  }
-  
-  export default NavBar;
+  return (
+    <div>
+      <ul id="nav">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/create-post">Create Post</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
