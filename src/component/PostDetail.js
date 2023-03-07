cfcf8b6983e3aad74357ffd620d5e7c678645d61
Sayan Ghosh <99132893+sayanghoshofficial@users.noreply.github.com>
2023-03-07 22:08:58 +0530

    add radium libray

--- src/component/PostDetail.js
+++ src/component/PostDetail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { firestore } from '../firebase';
+import Radium from 'radium';
 
 function PostDetail() {
   const [post, setPost] = useState({});
@@ -19,10 +20,26 @@ function PostDetail() {
 
   return (
     <div className="post-detail">
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
+      <h1 style={styles.heading}>{post.title}</h1>
+      <p style={styles.PostDetail}>{post.content}</p>
     </div>
   );
 }
 
-export default PostDetail;
+export default Radium(PostDetail);
+
+const styles = {
+  PostDetail: {
+    border: '1px solid #e1e1e1',
+    padding: 5,
+    paddingTop: 10,
+    borderRadius: 5,
+  },
+  heading: {
+    textAlign: 'center',
+    margin: 0,
+    ':hover': {
+      color: 'red',
+    },
+  },
+};
